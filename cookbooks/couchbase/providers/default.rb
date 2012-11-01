4d10c8e39694381da69cafcf29b5c20e57932de2
Ove Kargard <ove@rightscale.com>
2012-11-01 14:08:36 -0700

    metadata change

---
+++ cookbooks/couchbase/providers/default.rb
@@ -0,0 +1,24 @@
+# Cookbook Name:: couchbase
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+#
+
+# Stop couchbase
+action :stop do
+  bash do
+    #stop is broken
+    #service couchbase-server stop
+    
+    kill -15 `cat "/opt/couchbase/var/lib/couchbase/couchbase-server.pid"`
+    pkill -15 -u 101
+    
+  end
+end
+
+
+# Start couchbase
+action :start do
+  service couchbase-server stop
+end
---
+++ cookbooks/couchbase/resources/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: couchbase
+# Resource:: couchbase::default
+#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
+
+# Stop couchbase-server
+actions :stop
+# Start couchbase-server
+actions :start
