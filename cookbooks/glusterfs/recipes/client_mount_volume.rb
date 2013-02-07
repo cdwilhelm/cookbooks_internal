923893d891048da65782c13493395f1c562ea9e1
Tim Boesenberg <jojomnky@gmail.com>
2013-02-07 11:44:17 -0500

    revert back to a single volume not a pool.

--- cookbooks/glusterfs/recipes/client_mount_volume.rb
+++ cookbooks/glusterfs/recipes/client_mount_volume.rb
@@ -16,77 +16,75 @@ TAG_VOLUME    = node[:glusterfs][:tag][:volume]
 TAG_MOUNT     = node[:glusterfs][:tag][:mount]
 TAG_MOUNTED   = node[:glusterfs][:tag][:mounted]
 
-node[:glusterfs][:volume_pool].each do |volume|
-  VOL_NAME = volume
+VOL_NAME = node[:glusterfs][:volume_name]
 
-  # find all servers providing the volume we need
-  r = server_collection "glusterfs" do
-    tags "#{TAG_VOLUME}=#{VOL_NAME}"
-    action :nothing
-  end
-  r.run_action(:load)
-
-  # get the IP address of one of them (doesn't matter which one)
-  glusterfs_ip=""
-  r = ruby_block "Find Server IP" do
-    block do
-      node[:server_collection]["glusterfs"].each do |id, tags|
-        ip_tag = tags.detect { |t| t =~ /^server:private_ip_0=/ }
-        glusterfs_ip = ip_tag.gsub(/^.*=/, '')
-        break   # just need one
-      end
-    end
-    action :nothing
-  end
-  r.run_action(:create)
-
-  if glusterfs_ip.empty?
-      raise "!!!> Didn't find any servers with tag #{TAG_VOLUME}=#{VOL_NAME}"
-  else
-      log "===> Found GlusterFS server at #{glusterfs_ip}"
-  end
-
-  # load fuse module
-  bash "modprobe fuse" do
-    code <<-EOF
-      if modinfo fuse &>/dev/null; then
-        if grep -q fuse /proc/modules; then
-          echo "Fuse already loaded, skipping..."
-        else
-          echo "Fuse available but not loaded, running modprobe"
-          modprobe fuse
-        fi
-      fi
-    EOF
-    #only_if "modinfo fuse &>/dev/null && ! grep -q fuse /proc/modules"
-  end
-
-  # create mount point
-  log "===> Creating mount point #{MOUNT_POINT}/#{VOL_NAME}"
-  directory MOUNT_POINT+"/"+VOL_NAME do
-    recursive true
-  end
-
-  # mount remote filesystem
-  log "===> Mounting GlusterFS volume"
-  bash "mount_glusterfs" do
-    user "root"
-    code <<-EOF
-      opts=
-      [ -n "#{MOUNT_OPTS}" ] && opts="-o #{MOUNT_OPTS}"
-      mount -t glusterfs $opts #{glusterfs_ip}:/#{VOL_NAME} #{MOUNT_POINT}/#{VOL_NAME} 
-    EOF
-    not_if "/bin/grep -qw '#{MOUNT_POINT}/#{VOL_NAME}' /proc/mounts"
-  end
-
-  right_link_tag "#{TAG_MOUNTED}=true" do
-    action :publish
-  end
-
-  right_link_tag "#{TAG_MOUNT}=#{MOUNT_POINT}/#{VOL_NAME}" do
-    action :publish
-  end
-
+# find all servers providing the volume we need
+r = server_collection "glusterfs" do
+  tags "#{TAG_VOLUME}=#{VOL_NAME}"
+  action :nothing
 end
+r.run_action(:load)
+
+# get the IP address of one of them (doesn't matter which one)
+glusterfs_ip=""
+r = ruby_block "Find Server IP" do
+  block do
+    node[:server_collection]["glusterfs"].each do |id, tags|
+      ip_tag = tags.detect { |t| t =~ /^server:private_ip_0=/ }
+      glusterfs_ip = ip_tag.gsub(/^.*=/, '')
+      break   # just need one
+    end
+  end
+  action :nothing
+end
+r.run_action(:create)
+
+if glusterfs_ip.empty?
+    raise "!!!> Didn't find any servers with tag #{TAG_VOLUME}=#{VOL_NAME}"
+else
+    log "===> Found GlusterFS server at #{glusterfs_ip}"
+end
+
+# load fuse module
+bash "modprobe fuse" do
+  code <<-EOF
+    if modinfo fuse &>/dev/null; then
+      if grep -q fuse /proc/modules; then
+        echo "Fuse already loaded, skipping..."
+      else
+        echo "Fuse available but not loaded, running modprobe"
+        modprobe fuse
+      fi
+    fi
+  EOF
+  #only_if "modinfo fuse &>/dev/null && ! grep -q fuse /proc/modules"
+end
+
+# create mount point
+log "===> Creating mount point #{MOUNT_POINT}/#{VOL_NAME}"
+directory MOUNT_POINT+"/"+VOL_NAME do
+  recursive true
+end
+
+# mount remote filesystem
+log "===> Mounting GlusterFS volume"
+bash "mount_glusterfs" do
+  user "root"
+  code <<-EOF
+    opts=
+    [ -n "#{MOUNT_OPTS}" ] && opts="-o #{MOUNT_OPTS}"
+    mount -t glusterfs $opts #{glusterfs_ip}:/#{VOL_NAME} #{MOUNT_POINT}/#{VOL_NAME} 
+  EOF
+  not_if "/bin/grep -qw '#{MOUNT_POINT}/#{VOL_NAME}' /proc/mounts"
+end
+
+right_link_tag "#{TAG_MOUNTED}=true" do
+  action :publish
+end
+
+right_link_tag "#{TAG_MOUNT}=#{MOUNT_POINT}/#{VOL_NAME}" do
+  action :publish
+end
+
 rightscale_marker :end
 
