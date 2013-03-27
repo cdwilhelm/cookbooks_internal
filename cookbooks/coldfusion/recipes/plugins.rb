8c48ef22038d9834b2ca64bdecaeb7e817e9651b
Tim Boesenberg <jojomnky@gmail.com>
2013-03-27 15:54:48 -0400

    yet another attempt

--- cookbooks/coldfusion/recipes/plugins.rb
+++ cookbooks/coldfusion/recipes/plugins.rb
@@ -7,14 +7,13 @@ remote_file "#{node[:coldfusion][:jar_dir]}/jedis-2.1.0.jar" do
  mode 00644
 end
 
-remote_file "#{Chef::Config['file_cache_path']}/#{node[:coldfusion][:tarball]}" do
- owner 'root'
- group 'root'
- source node[:coldfusion][:commons_url]
- mode 00644
+bash 'extract_module' do
+  cwd "/tmp"
+  code <<-EOH
+    wget #{node[:coldfusion][:commons_url]}
+    tar xzf #{src_filename}
+    EOH
 end
-
-system "tar zxf #{Chef::Config['file_cache_path']}/#{node[:coldfusion][:tarball]}"
 
 file "#{node[:coldfusion][:jar_dir]}/commons-pool-1.6.jar" do
   content IO.read("#{Chef::Config['file_cache_path']}/commons-pool-1.6/commons-pool-1.6.jar")
