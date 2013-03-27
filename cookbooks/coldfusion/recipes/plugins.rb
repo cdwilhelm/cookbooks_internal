71bb746b5c67f45d4b59b9c33bceaf9065d8c9b2
Tim Boesenberg <jojomnky@gmail.com>
2013-03-27 15:38:56 -0400

    using #{Chef::Config['file_cache_path']}

--- cookbooks/coldfusion/recipes/plugins.rb
+++ cookbooks/coldfusion/recipes/plugins.rb
@@ -2,18 +2,22 @@ rightscale_marker :begin
 
 remote_file "#{node[:coldfusion][:jar_dir]}/jedis-2.1.0.jar" do
  source node[:coldfusion][:jedis_url]
- mode "0644"
+ owner 'root'
+ group 'root'
+ mode 00644
 end
 
-remote_file "/tmp/#{node[:coldfusion][:tarball]}" do
+remote_file "#{Chef::Config['file_cache_path']}/#{node[:coldfusion][:tarball]}" do
+ owner 'root'
+ group 'root'
  source node[:coldfusion][:commons_url]
- mode "0644"
+ mode 00644
 end
 
-system "tar zxf /tmp/#{node[:coldfusion][:tarball]}"
+system "tar zxf #{Chef::Config['file_cache_path']}/#{node[:coldfusion][:tarball]}"
 
 file "#{node[:coldfusion][:jar_dir]}/commons-pool-1.6.jar" do
-  content IO.read("/tmp/commons-pool-1.6/commons-pool-1.6.jar")
+  content IO.read("#{Chef::Config['file_cache_path']}/commons-pool-1.6/commons-pool-1.6.jar")
 end
 
 rightscale_marker :end
