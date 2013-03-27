5ef7e769ba78733935dcdf28f05a9b66585000ae
Tim Boesenberg <jojomnky@gmail.com>
2013-03-27 15:23:33 -0400

    back to normal taring

--- cookbooks/coldfusion/recipes/plugins.rb
+++ cookbooks/coldfusion/recipes/plugins.rb
@@ -4,13 +4,13 @@ remote_file "#{node[:coldfusion][:jar_dir]}/jedis-2.1.0.jar" do
  source node[:coldfusion][:jedis_url]
  mode "0644"
 end
-puts `pwd`
+
 remote_file "/tmp/#{node[:coldfusion][:tarball]}" do
  source node[:coldfusion][:commons_url]
  mode "0644"
 end
 
-system "tar -C /tmp zxf /tmp/#{node[:coldfusion][:tarball]}"
+system "tar zxf /tmp/#{node[:coldfusion][:tarball]}"
 
 file "#{node[:coldfusion][:jar_dir]}/commons-pool-1.6.jar" do
   content IO.read("/tmp/commons-pool-1.6/commons-pool-1.6.jar")
