908a19591ecc1975ed704d20130747c6625d8f5d
Tim Boesenberg <jojomnky@gmail.com>
2013-03-28 15:12:33 -0400

    trying chmod

--- cookbooks/coldfusion/recipes/install.rb
+++ cookbooks/coldfusion/recipes/install.rb
@@ -19,6 +19,6 @@ template "/tmp/cf902silent.properties" do
   )
 end
 
-system "sudo /tmp/#{node[:coldfusion][:s3][:file_prefix]}.bin -f /tmp/cf902silent.properties"
+system "chmod 777 /tmp/#{node[:coldfusion][:s3][:file_prefix]}.bin; sudo /tmp/#{node[:coldfusion][:s3][:file_prefix]}.bin -f /tmp/cf902silent.properties"
 
 rightscale_marker :end
