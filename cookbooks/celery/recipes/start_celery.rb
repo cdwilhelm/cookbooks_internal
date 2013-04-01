80298870ae39b468c248faf877fdf40002c695ee
Tim Boesenberg <jojomnky@gmail.com>
2013-04-01 14:15:37 -0400

    detach from celerey daemon

--- cookbooks/celery/recipes/start_celery.rb
+++ cookbooks/celery/recipes/start_celery.rb
@@ -2,7 +2,7 @@ rightscale_marker :begin
 
 ruby_block "start celery" do
   block do
-    system "celeryd --pidfile=/var/run/celery.pid"
+    system "celeryd --pidfile=/var/run/celery.pid &"
   end
 end
 rightscale_marker :end
