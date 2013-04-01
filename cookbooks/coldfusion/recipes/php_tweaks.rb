ff989e07a56c7c684b9d23d536bdd81578c46b99
Tim Boesenberg <jojomnky@gmail.com>
2013-04-01 11:56:05 -0400

    adds centos case for apc install

--- cookbooks/coldfusion/recipes/php_tweaks.rb
+++ cookbooks/coldfusion/recipes/php_tweaks.rb
@@ -1,6 +1,14 @@
 rightscale_marker :begin
 
-package "php-apc"
+case node[:platform]
+when "ubuntu","debian"
+  package "php-apc" do
+    action :install
+  end
+when "centos"
+  package "php53-devel"
+  package "php-pecl-apc"
+end
 
 execute "symfony submodules" do
  installation_dir = "/home/webapps/#{node[:coldfusion][:application]}"
