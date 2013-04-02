75ea672ed00ac57b7ce54dfd4b226b7d60ece7f8
Tim Boesenberg <jojomnky@gmail.com>
2013-04-02 08:40:35 -0400

    adds dependency for web_apache

--- cookbooks/coldfusion/metadata.rb
+++ cookbooks/coldfusion/metadata.rb
@@ -6,6 +6,7 @@ version          "0.0.1"
 
 depends "rightscale"
 depends "block_device"
+depends "web_apache"
 
 recipe "coldfusion::install", "Installs Coldfusion"
 recipe "coldfusion::plugins", "Adds Redis support"
