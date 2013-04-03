41261dae54939569b05c3d179a396f0a5decd045
Tim Boesenberg <jojomnky@gmail.com>
2013-04-03 14:15:42 -0400

    added redis.yml template

--- cookbooks/coldfusion/recipes/redis_credentials.rb
+++ cookbooks/coldfusion/recipes/redis_credentials.rb
@@ -8,4 +8,11 @@ template "/home/webapps/#{node[:coldfusion][:application]}/includes/www/logic/re
   )
 end
 
+template "/home/webapps/#{node[:coldfusion][:application]}/symfony/config/redis.yml" do
+  source "redis.yml.erb"
+  variables(
+    :hostname => node[:coldfusion][:redis][:hostname],
+    :password => node[:coldfusion][:redis][:password]
+  )
+end
 rightscale_marker :end
