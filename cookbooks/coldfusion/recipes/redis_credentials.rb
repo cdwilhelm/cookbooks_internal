949172e142524e544f0f0d57714b086e07b493fe
Tim Boesenberg <jojomnky@gmail.com>
2013-04-04 08:00:24 -0400

    set redis.yml to readable

--- cookbooks/coldfusion/recipes/redis_credentials.rb
+++ cookbooks/coldfusion/recipes/redis_credentials.rb
@@ -2,6 +2,7 @@ rightscale_marker :begin
 
 template "/home/webapps/#{node[:coldfusion][:application]}/includes/www/logic/redis.cfm" do
   source "redis.cfm.erb"
+  mode 00644
   variables(
     :hostname => node[:coldfusion][:redis][:hostname],
     :password => node[:coldfusion][:redis][:password]
@@ -10,6 +11,7 @@ end
 
 template "/home/webapps/#{node[:coldfusion][:application]}/symfony/config/redis.yml" do
   source "redis.yml.erb"
+  mode 00644
   variables(
     :hostname => node[:coldfusion][:redis][:hostname],
     :password => node[:coldfusion][:redis][:password]
