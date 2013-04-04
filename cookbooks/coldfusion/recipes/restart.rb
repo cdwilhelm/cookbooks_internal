a0a08d93a4258c1ea66a279cbdffdb4b9b760a20
Tim Boesenberg <jojomnky@gmail.com>
2013-04-04 13:16:27 -0400

    manually stops and starts coldfusion

--- cookbooks/coldfusion/recipes/restart.rb
+++ cookbooks/coldfusion/recipes/restart.rb
@@ -1,6 +1,26 @@
 rightscale_marker :begin
 
-include_recipe "coldfusion::stop"
-include_recipe "coldfusion::start"
+File.unlink '/tmp/cf.log' if File.exists?('/tmp/cf.log');
+
+ruby_block "stop cf" do
+  block do
+    system "/opt/jrun4/bin/jrun stop cfusion"
+  end
+end
+
+ruby_block "start cf" do
+  block do
+    system "/opt/jrun4/bin/jrun start cfusion >2 /tmp/cf.log &"
+  end
+end
+
+x = 0
+until File.exists?('/tmp/cf.log') && open('/tmp/cf.log') { |f| f.grep(/Server cfusion ready/) }
+  x += 1
+  sleep(1)
+  if x == 300
+    break
+  end
+end
 
 rightscale_marker :end
