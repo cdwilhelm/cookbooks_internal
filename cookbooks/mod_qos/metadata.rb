db4e09aeaa6f9d66775db61685bc3b2478725477
Tim Boesenberg <jojomnky@gmail.com>
2013-04-12 09:59:54 -0400

    adds mod_qos

---
+++ cookbooks/mod_qos/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "School Spring, Inc."
+maintainer_email "devteam@schoolspring.com"
+license          "Apache 2.0"
+description      "mod_qos Recipes" 
+version          "0.0.1"
+
+depends "rightscale"
+depends "web_apache"
+
+recipe "mod_qos::install", "Installs mod_qos"
---
+++ cookbooks/mod_qos/recipes/install.rb
@@ -0,0 +1,25 @@
+rightscale_marker :begin
+
+package openssl-evel
+package pcre-devel
+package httpd-devel
+
+cookbook_file "/tmp/mod_qos-10.14.tar.gz" do
+  source "mod_qos-10.14.tar.gz"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+bash 'extract module' do
+  cwd "/tmp"
+  code <<-EOH
+    tar xzf /tmp/mod_qos-10.14.tar.gz
+    apxs -i -c mod_qos-10.14/apache2/mod_qos.c
+    echo "LoadModule qos_module /usr/lib64/httpd/modules/mod_qos.so" >> /etc/httpd/conf/httpd.conf
+    EOH
+  not_if { File.exists?("/usr/lib64/httpd/modules/mod_qos.so") }
+  notifies :restart, resources(:service => "apache2")
+end
+
+rightscale_marker :end
