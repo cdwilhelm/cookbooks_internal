8682d4f20bfa720fe83affa014bf9b997768f313
Tim Boesenberg <jojomnky@gmail.com>
2013-04-22 11:11:12 -0400

    adds perl recipe

---
+++ cookbooks/perl/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "SchoolSpring.com"
+maintainer_email "devteam@schoolspring.com"
+license          "Apache 2.0"
+description      "Perl" 
+version          "0.0.1"
+
+depends "sys"
+
+recipe "perl::add_modules", "Adds Perl Modules"
---
+++ cookbooks/perl/recipes/add_modules.rb
@@ -0,0 +1,10 @@
+rightscale_marker :begin
+
+package "libdbi-perl"
+package "libmail-sender-perl"
+package "libimage-magick-perl"
+package "libnet-amazon-s3-perl"
+package "libwww-mechanize-perl"
+package "libdata-dump-perl"
+
+rightscale_marker :end
