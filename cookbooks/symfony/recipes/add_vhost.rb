8bbdbfc0f612f8465ff12141a2fc730cb886779d
Tim Boesenberg <jojomnky@gmail.com>
2013-07-03 14:46:44 -0400

    stopped using notifies

--- cookbooks/symfony/recipes/add_vhost.rb
+++ cookbooks/symfony/recipes/add_vhost.rb
@@ -7,20 +7,6 @@ node[:web_app] = JSON.parse(node[:web_app_config])
 
 RightScale::Repo::GitSshKey.new.create(node[:repo][:default][:credential], node[:repo][:default][:credential] )
 
-execute "composer_install" do
-  cwd "/home/webapps/#{node[:web_app][:application]}/symfony2/"
-  command "php composer.phar install"
-  only_if { ::File.exists?("/home/webapps/#{node[:web_app][:application]}/symfony2/composer.phar") }
-  action :nothing
-end
-
-execute "clear_cache" do
-  cwd "/home/webapps/#{node[:web_app][:application]}"
-  command "app/console cache:clear"
-  only_if { ::File.exists?("/home/webapps/#{node[:web_app][:application]}/symfony2/app/console") }
-  action :nothing
-end
-
 
 log "===> Cloning resource"
 git "/home/webapps/#{node[:web_app][:application]}" do
@@ -28,9 +14,22 @@ git "/home/webapps/#{node[:web_app][:application]}" do
   reference node[:web_app][:git_revision]
   enable_submodules node[:web_app][:submodule_init]
   action :sync
-  notifies :run, "execute[composer_install]", :immediately
-  notifies :run, "execute[clear_cache]"
 end
+
+execute "composer_install" do
+  cwd "/home/webapps/#{node[:web_app][:application]}/symfony2/"
+  command "php composer.phar install"
+  only_if { ::File.exists?("/home/webapps/#{node[:web_app][:application]}/symfony2/composer.phar") }
+  action :run
+end
+
+execute "clear_cache" do
+  cwd "/home/webapps/#{node[:web_app][:application]}"
+  command "app/console cache:clear"
+  only_if { ::File.exists?("/home/webapps/#{node[:web_app][:application]}/symfony2/app/console") }
+  action :run
+end
+
 
 if node[:web_app][:htpasswd]
   execute "htpasswd" do 
