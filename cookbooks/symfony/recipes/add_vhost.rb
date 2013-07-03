03395bf35215f46dbd34c23b3328b8555a2edebf
Tim Boesenberg <jojomnky@gmail.com>
2013-07-03 11:56:11 -0400

    override prompt for htpassword creation

--- cookbooks/symfony/recipes/add_vhost.rb
+++ cookbooks/symfony/recipes/add_vhost.rb
@@ -22,7 +22,7 @@ end
 
 if node[:web_app][:htpasswd]
   execute "htpasswd" do 
-    command "htpasswd -c /home/webapps/#{node[:web_app][:application]}/.htpasswd #{node[:web_app][:htpasswd][:username]} #{node[:web_app][:htpasswd][:username]}"
+    command "htpasswd -c -b /home/webapps/#{node[:web_app][:application]}/.htpasswd #{node[:web_app][:htpasswd][:username]} #{node[:web_app][:htpasswd][:username]}"
   end
 end
 template "/home/webapps/#{node[:web_app][:application]}/Symfony2/app/config/parameters.yml" do
