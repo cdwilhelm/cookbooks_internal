92c996e2447f3bd3a4796f51e69dffcc6daee079
Tim Boesenberg <jojomnky@gmail.com>
2013-07-10 09:37:51 -0400

    keyserver added

--- cookbooks/symfony/recipes/add_vhost.rb
+++ cookbooks/symfony/recipes/add_vhost.rb
@@ -32,9 +32,15 @@ end
 
 apt_repository "nodejs" do
   uri "http://ppa.launchpad.net/chris-lea/node.js"
+  components ["main","stable"]
+  keyserver "keyserver.ubuntu.com"
+  key "C7917B12"
 end
 apt_repository "nodejs" do
   uri "http://ppa.launchpad.net/ondrej/php5"
+  components ["main","stable"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
 end
 execute "php_add_package" do
   command "apt-get update"
