ca58cd2e539063fcccd6a17a02c14bc0aa15b623
Tim Boesenberg <jojomnky@gmail.com>
2013-08-15 15:14:22 -0400

    added testin code for pwd

--- cookbooks/symfony/recipes/update_vendors.rb
+++ cookbooks/symfony/recipes/update_vendors.rb
@@ -9,6 +9,7 @@ RightScale::Repo::GitSshKey.new.create(node[:repo][:default][:credential], node[
 
 execute "composer_install" do
   cwd "/home/webapps/#{node[:web_app][:application]}#{node[:web_app][:symfony_dir]}"
+  command "pwd"
   command "php composer.phar update #{node[:composer][:arguments]}"
   only_if { ::File.exists?("/home/webapps/#{node[:web_app][:application]}#{node[:web_app][:symfony_dir]}composer.phar") }
   action :run
