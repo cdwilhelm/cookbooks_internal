82929c8ed1b9f311cc0b84de96623de5e2e9f718
Curt Wilhelm <curt@rightscale.com>
2014-11-20 09:42:40 -0700

    install java 1.7

---
+++ cookbooks/coldfusion/recipes/install_java.rb
@@ -0,0 +1,15 @@
+
+rightscale_marker
+log "installing Java 1.7"
+
+case  node[:platform]
+when "centos|redhat"
+ packages=["java-1.7.0-openjdk"]
+when "ubuntu"
+ packages=["openjdk-7-jre"]
+done
+
+packages.each do |p|
+ package p
+end
+
