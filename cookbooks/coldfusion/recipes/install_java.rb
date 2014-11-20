0dcff77b57adf07a1bc4ec3585ff96e1739f1a33
Curt Wilhelm <curt@rightscale.com>
2014-11-20 11:21:20 -0700

    install jdk and set alternatives

--- cookbooks/coldfusion/recipes/install_java.rb
+++ cookbooks/coldfusion/recipes/install_java.rb
@@ -6,10 +6,22 @@ case  node[:platform]
 when "centos|redhat"
  packages=["java-1.7.0-openjdk"]
 when "ubuntu"
- packages=["openjdk-7-jre"]
+ packages=["openjdk-7-jdk", "icedtea-7-plugin"]
 end
 
 packages.each do |p|
  package p
 end
 
+case node[:platform]
+when "centos|redhat"
+# execute "update-java-alternatives" do
+#   command "update-java-alternatives -s java-1.7.0-openjdk-amd64"
+#   action :run
+# end
+when "ubuntu"
+ execute "update-java-alternatives" do
+   command "update-java-alternatives -s java-1.7.0-openjdk-amd64"
+   action :run
+ end
+end
