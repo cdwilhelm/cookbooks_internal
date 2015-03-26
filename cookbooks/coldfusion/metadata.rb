77db21ddc2ffeb9c3e2b2a9c038eac9cbff94511
JoJo <jojomnky@gmail.com>
2015-03-26 17:42:53 -0400

    Update metadata.rb
    bump version

--- cookbooks/coldfusion/metadata.rb
+++ cookbooks/coldfusion/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "School Spring, Inc."
 maintainer_email "devteam@schoolspring.com"
 license          "Apache 2.0"
 description      "ColdFusion Recipes" 
-version          "0.0.6"
+version          "0.0.7"
 
 depends "rightscale"
 depends "block_device"
